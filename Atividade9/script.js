98d70f391ae5ba9fca9f4ba9cae994160b62a6a9
Pedro Tortello <47224987+PTortello@users.noreply.github.com>
2021-09-29 21:14:51 -0300

    Add files via upload

---
+++ Atividade9/script.js
@@ -0,0 +1,10 @@
+var maior = (n1, n2, n3) => {
+  return Math.max(n1, n2, n3);
+}
+
+var crescente = (n1, n2, n3) => {
+  return [n1, n2, n3].sort();
+}
+
+console.log(maior(5, 3, 4));
+console.log(crescente(5, 3, 4));
